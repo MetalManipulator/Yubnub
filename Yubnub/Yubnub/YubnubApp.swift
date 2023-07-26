5707d2272818fb47229166ebb4a420f44a29ba7f
Levi Gustin <levigustin@gmail.com>
2023-07-25 21:26:08 -0600

    Creation of project

---
+++ Yubnub/Yubnub/YubnubApp.swift
@@ -0,0 +1,17 @@
+//
+//  YubnubApp.swift
+//  Yubnub
+//
+//  Created by Levi Gustin on 7/25/23.
+//
+
+import SwiftUI
+
+@main
+struct YubnubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
